a8ec3b62481afe6f43d1f68b4e764e2fceda7e5b
ProsenkoN <player.neci3@gmail.com>
2026-04-03 09:24:38 +0000

    alternative

--- app.py
+++ app.py
@@ -9,14 +9,20 @@ notes_table=db.table('notes')
 
 @app.route('/')
 def zapiski():
-    if "user" in session:
-        redirect("/index.html")
-    return redirect("/login")
-return render_template("register.html")
-       
+    if "user" not in session:
+        #redirect("/index.html")
+        return redirect("/login")
+    user = users.get(User.username == session["user"])
+    note=user.get('note'," ")
+    #return render_templates("register.html")
+    return render_template('index.html',notes=notes, note=note, uporabnik=session["user"])
 #return render_template('index.html',notes=notes)
 
-
+@app.route("/nekej",methods=["POST"])
+def nekej():
+    note = request.form[note]
+    print(note)
+    users.update(["note" : note], User.username == session["user"])
 @app.route('/login', methods=["GET","POST"])
 def login():
     if request.method == 'POST':
@@ -39,9 +45,9 @@ def login():
 
 @app.route('/register',methods=["GET","POST"])
 
-@app.route('/logout',methods=["GET","POST"])
+@app.route('/logout')
 def logout():
     session.clear()
-
+    return redirect("/login")
 
 app.run(debug=True)
